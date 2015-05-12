34800b4eb91573ca6eaf24c56eb0855cb7be45ee
Konosprod <konosprod@free.fr>
2015-05-12 17:22:40 +0200

    Move file depending on the path associated with the filename

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 ﻿import socketserver
 import json
+import shutil
+import atexit
+
+files = dict()
 
 class MyTCPServer(socketserver.ThreadingTCPServer):
 	allow_reuse_address = True
@@ -11,15 +15,25 @@ class MyTCPServerHandler(socketserver.BaseRequestHandler):
 			if data['type'] == "add":
 				addFile(data['path'], data['filename'])
 			elif data['type'] == "move":
-				moveFile(data['path', data['filename'])
+				moveFile(data['path'], data['filename'])
+			elif data['type'] == "remove":
+				
 			
 def moveFile(path, filename):
-	print(path + filename)
+	for k in files.keys():
+		if files[k] in filename:
+			shutil.move(path, k + "/" + filename)
 	
 def addFile(path, filename):
-	
+	files[path] = filename
 
-		
-		
+def loadDict():
+	return 0
+	
+def writeDict():
+	return 0
+
 server = MyTCPServer(('127.0.0.1', 13373), MyTCPServerHandler)
+loadDict()
+atexit.register(writeDict)
 server.serve_forever()
