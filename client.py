59413f72a9d3e3b916bc21ddd3dbe03a9778f55e
Konosprod <konosprod@free.fr>
2015-05-12 17:03:02 +0200

    Add new file moving scheme to the dictionary

---
+++ client.py
@@ -0,0 +1,28 @@
+﻿import socket
+import json
+import sys
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 13373))
+
+def sendAdd(path, filename):
+	global s
+	
+	data  = {"type" : "add", "path" : path, "filename" : filename}
+	
+	s.send(bytes(json.dumps(data), 'UTF-8'))
+	
+	
+def sendMove(path, filename):
+	data  = {"type" : "move", "path" : path, "filename" : filename}
+	
+	s.send(bytes(json.dumps(data), 'UTF-8'))
+
+	
+if __name__ == "__main__":
+	if sys.argv[1] == 'add':
+		sendAdd(sys.argv[2], sys.argv[3])
+	elif sys.argv[1] == 'move':
+		sendMove(sys.argv[2], sys.argv[3])
+		
+	s.close()
---
+++ server.py
@@ -0,0 +1,25 @@
+﻿import socketserver
+import json
+
+class MyTCPServer(socketserver.ThreadingTCPServer):
+	allow_reuse_address = True
+
+class MyTCPServerHandler(socketserver.BaseRequestHandler):
+	def handle(self):
+		data = json.loads(self.request.recv(1024).decode('UTF-8').strip())
+		if 'type' in data:
+			if data['type'] == "add":
+				addFile(data['path'], data['filename'])
+			elif data['type'] == "move":
+				moveFile(data['path', data['filename'])
+			
+def moveFile(path, filename):
+	print(path + filename)
+	
+def addFile(path, filename):
+	
+
+		
+		
+server = MyTCPServer(('127.0.0.1', 13373), MyTCPServerHandler)
+server.serve_forever()
